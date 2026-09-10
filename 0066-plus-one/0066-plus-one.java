39d33b4e0e53681a4192504a542eee7ac7503dfb
alvira100 <alvira1904@gmail.com>
2026-09-10 13:11:37 +0530

    Time: 0 ms (100%), Space: 43.6 MB (23.83%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] plusOne(int[] a) {
+        for(int i = a.length-1; i >= 0; i--) {
+            if(a[i] < 9) { a[i]++; return a; }
+            a[i] = 0;
+        }
+        int[] r = new int[a.length + 1];
+        r[0] = 1;
+        return r;
+    }
+}
